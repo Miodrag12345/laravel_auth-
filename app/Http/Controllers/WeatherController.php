32310dc40619c56285e2635a861f5efcfc416ba5
Miodrag12345 <realsosijedad@gmail.com>
2025-07-25 01:03:19 +0200

    #Ovo je 9 domaci zadatak

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WeatherController extends Controller
+{
+    public function index()
+    {
+        $prognoza=[
+            "Beograd"=>22,
+            "Sarajevo"=>24,
+            "Novi Sad"=>23,
+            "Zagreb"=>26
+        ];
+        return view("weather", compact ("prognoza")); //ime stranice u get sto smo ubacili nas url
+    }
+}
