c28e4342e1905a4909438d41f246f0ed48ce414c
Miodrag12345 <realsosijedad@gmail.com>
2025-08-11 18:32:27 +0200

    #Added Weather Seeder

---
+++ database/seeders/UserWeatherSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\WeatherModel;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserWeatherSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        $city=$this->command->getOutput()->ask("Unesite ime grada");
+
+        if($city=== null){
+            $this->command->getOutput()->error("Niste uneli ime grada");
+        }
+        $temperature=$this->command->getOutput()->ask("Unesite temperaturu grada");
+        if($temperature===null){
+            $this->command->getOutput()->error("Niste uneli remperaturu grada");
+        }
+        WeatherModel::create([
+                'city'=>$city,
+                'temperature'=>$temperature
+            ]
+
+        );
+        $this->command->getOutput()->info("Uspesno smo uneli novi grad $city sa temperatutom $temperature");
+    }
+}
