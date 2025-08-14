506ef2ce545e721493eb2d0ee5851bf7cf8e5602
Miodrag12345 <realsosijedad@gmail.com>
2025-08-14 19:29:53 +0200

    #Added UserSeeder domaci 12 5 deo

---
+++ database/seeders/ForecastSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\CitiesModel;
+use App\Models\ForecastModel;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ForecastSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $cities=CitiesModel::all();
+        foreach($cities as $city){
+            for($i=0; $i<5; $i++){
+                ForecastModel::create([
+                    "city_id"=>$city->id,
+                    "temperature"=>"22.22",
+                    "forecast_date"=>Carbon::now()
+                ]);
+            }
+        }
+    }
+}
