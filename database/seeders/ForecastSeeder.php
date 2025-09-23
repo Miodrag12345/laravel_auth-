91566043c3e8ba6ead4f37fd25be74a86dc08002
Miodrag12345 <realsosijedad@gmail.com>
2025-09-23 19:06:21 +0200

    Domaci 15 lekcija

--- database/seeders/ForecastSeeder.php
+++ database/seeders/ForecastSeeder.php
@@ -15,20 +15,49 @@ class ForecastSeeder extends Seeder
     public function run(): void
     {
         $cities=CitiesModel::all();
+
         foreach($cities as $city){
+            $lastTemperature=null;
+
             for($i=0; $i<5; $i++){
                 $weatherType= ForecastModel::WEATHERS[rand(0,3)]; // izvadili smo weather type
                 $probability=null; // po defaltu
                 if($weatherType=="rainy" || $weatherType=="snowy"){
                     $probability=rand(1,100);
                 }
+
+                $temperature=null;
+
+                if($lastTemperature !== null){
+                    $minTemperature=$lastTemperature-5;
+                    $maxTemperature=$lastTemperature+5;
+                    $temperature=rand($minTemperature,$maxTemperature);
+                } else {
+
+                switch ($weatherType){
+                    case "sunny";
+                    $temperature= rand(-50,50);
+                    break;
+                    case "cloudy";
+                    $temperature= rand(-50,15);
+                    break;
+                    case "rainy";
+                    $temperature= rand(-10,50);
+                    break;
+                    case "snowy";
+                    $temperature= rand(-50,1);
+                    break;
+
+                }}
+
                 ForecastModel::create([
                     "city_id"=>$city->id,
-                    "temperature"=>rand(15,30),
+                    "temperature"=>$temperature,
                     "forecast_date"=>Carbon::now()->addDays(rand(1,30)),
                     "weather_type"=>$weatherType,
                     "probability"=>$probability
                 ]);
+                $lastTemperature=$temperature; // da je poslednja temperatura grada definisana jednaka trenutnoj temperaturi
             }
         }
     }
