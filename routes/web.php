1b1812eb04348ccdba1311ff788d4c2da11e3892
Miodrag12345 <realsosijedad@gmail.com>
2025-09-25 20:17:07 +0200

    16 lekcija part 5

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WeatherController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::view("/","welcome");
 
 Route::get('/dashboard', function () {
     return view('dashboard');
